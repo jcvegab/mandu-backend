22f19c020639ee5c48d746ae11110de3a3b037de
Joseph Vega <jcvb0206@gmail.com>
2021-07-18 21:12:03 -0500

    Seed de Usuarios completo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
-use App\Models\User;
+use \App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        
+
         $this->call(DivisionSeeder::class);
+        User::factory(100)->create();
     }
 }
